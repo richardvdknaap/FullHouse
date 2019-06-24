a32e1e2d02b8cabfb61e42188e6eef7741de68f3
Richard <richard.vd.knaap@outlook.com>
2019-06-24 17:25:41 +0200

    FINAL THINGY

--- src/ToernooiSpelers.java
+++ src/ToernooiSpelers.java
@@ -9,6 +9,8 @@ public class ToernooiSpelers {
 
     public ToernooiSpelers(Object id){
 
+        Object idT = id;
+
         DbConnect connect = new DbConnect();
         JFrame f = new JFrame("Menu");
         JPanel p1 = new JPanel(new GridBagLayout());
@@ -35,14 +37,34 @@ public class ToernooiSpelers {
                 new Toernooi();
             }
         });
+        JButton b1 = new JButton("#1");
+        b1.setPreferredSize(new Dimension(100,50));
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int srow = table.getSelectedRow();
+                Object sid = table.getValueAt(srow, 0);
+                connect.winnaar(sid,1,idT);
+            }
+        });
+        JButton b2 = new JButton("#2");
+        b2.setPreferredSize(new Dimension(100,50));
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int srow = table.getSelectedRow();
+                Object sid = table.getValueAt(srow, 0);
+                connect.winnaar(sid,2,idT);
+            }
+        });
 
         c.anchor = GridBagConstraints.WEST;
 
         c.insets = new Insets(0,10,5,10);
         c.gridx = 0;c.gridy = 0;c.weightx = 0.5;p1.add(scrollPane,c);
-
-        c.insets = new Insets(10,0,0,150);
-        c.gridx = 0;c.gridy = 1;c.weightx = 0.5;p3.add(back,c);
+        c.insets = new Insets(0,0,5,0);
+        c.gridx = 0;c.gridy = 1;c.weightx = 0.5;p3.add(b1,c);
+        c.insets = new Insets(0,0,5,10);
+        c.gridx = 1;c.gridy = 1;c.weightx = 0.5;p3.add(b2,c);
+        c.gridwidth = 2;c.gridx = 0;c.gridy = 2;c.weightx = 0.5;p3.add(back,c);
 
         f.add(p1,BorderLayout.WEST);
         f.add(p2,BorderLayout.PAGE_START);
